752ff83fd46fad4d26d830fcbcfef19b4e1175db
semayank <setiyamayank30@gmail.com>
2024-03-08 21:28:46 +0530

    mmilonga started

--- functions/api/hello.js
+++ functions/api/hello.js
@@ -1,6 +1,6 @@
 export async function onRequestPost({ request }) {
 	try {
-	console.log("hello");
+	new Response("Data Received successfully",{status:200});
 	} catch (err) {
 		return new Response('Error parsing JSON content', { status: 400 });
 	}
